64aed4ae438fe301170b0b9292481dcc1000e1fe
newbi462 <isthisemailfree@gmail.com>
2020-01-21 16:22:39 -0500

    READ/GET endpoint

--- routes/carsRouter.js
+++ routes/carsRouter.js
@@ -5,7 +5,20 @@ const db = require('./../data/db.js');
 const router = express.Router();
 
 //GET
-
+router.get('/', (request, responce) => {
+  db.select('*').from("cars")
+    .then(cars => {
+      responce.json(cars);
+    })
+    .catch( error => {
+      console.log(error);
+      responce.status(500).json(
+        {
+          error: "Get Failed."
+        }
+      )
+    })
+});
 
 //POST
 
